b853e55edb84999db3dd8f2858b990fcf303fc4c
lucy3 <luxisufeili@gmail.com>
2018-05-18 23:59:58 -0700

    small progress

--- practice/R/add_metadata.R
+++ practice/R/add_metadata.R
@@ -1,19 +1,42 @@
 # Get some interesting characteristics of words
-library(rJava)
-library(wordnet)
-filter = getTermFilter("ExactMatchFilter", "cat", TRUE)
-filter
-terms = getIndexTerms("ADJECTIVE", 5, filter)
-synsets = getSynsets(terms[[1]])
-related = getRelatedSynsets(synsets[[1]], "!")
-sapply(related, getWord)
-
 longwords = reformat$longWord[1:40] 
 shortwords = reformat$shortWord[1:40]
 all_words = c(longwords, shortwords)
 
-syllables = vector(mode="list", length=80)
+syllables = c(3, 3, 2, 4, 3, 
+              4, 5, 3, 4, 5, 
+              5, 5, 4, 3, 3, 
+              5, 2, 3, 4, 3,
+              3, 4, 3, 3, 4, 
+              3, 4, 6, 3, 5, 
+              5, 4, 5, 3, 3, 
+              4, 5, 5, 5, 4, 
+              1, 2, 1, 1, 1, 
+              2, 3, 2, 2, 2, 
+              2, 1, 1, 1, 1, 
+              2, 1, 1, 1, 2, 
+              1, 1, 1, 2, 2, 
+              1, 1, 2, 1, 2, 
+              1, 1, 2, 1, 2, 
+              2, 2, 1, 2, 1)
 num_chars = vector(mode="list", length=80)
+# http://ucrel.lancs.ac.uk/bncfreq/lists/1_2_all_freq.txt
+freq_file = file("/Users/lucyli/Documents/Senior/ling245all/ling245exp/practice/data/1_2_all_freq.txt", "r")
+c = 0
+while ( TRUE ) {
+  line = readLines(freq_file, n = 1)
+  if ( length(line) == 0 ) {
+    break
+  }
+  l = unlist(strsplit(line, '\t'))
+  if (l[2] %in% all_words) {
+    print(l[2])
+    print(l[4])
+    c = c + 1
+  }
+}
+print(c)
+close(freq_file)
 freq = vector(mode="list", length=80)
 ambiguity = vector(mode="list", length=80)
 for (word in all_words) { 
